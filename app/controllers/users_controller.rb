482e5223e54cf96628f63f3eb5430405b45f154b
Mandeep Khera <world.mover@gmail.com>
2011-05-07 12:03:21 +0000

    finished updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,5 +21,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+      @user = User.find(params[:id])
+      @title = "Edit user" 
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "User updated"
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+  def index
+    @title = "All users"
+    @users = User.all
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end 
 
 end
